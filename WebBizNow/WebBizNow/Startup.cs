d877096d8d70d791adde57c40efc76b7f3215bc0
dj.jellings <dj.jellings@collegeamerica.edu>
2017-04-05 13:17:29 -0600

    what the fuck

---
+++ WebBizNow/WebBizNow/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebBizNow.Startup))]
+namespace WebBizNow
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
